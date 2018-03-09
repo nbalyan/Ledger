098a173c311a332e37109e7abf804f1423f382e4
Neeraj <bboy.d.end@gmail.com>
2018-03-09 16:06:54 +0530

    admin activity table

--- app/src/main/java/btventures/ledger/AdminActivity.java
+++ app/src/main/java/btventures/ledger/AdminActivity.java
@@ -1,5 +1,6 @@
 package btventures.ledger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        Intent homeActivity = new Intent(getApplicationContext(), TableActivity.class);
+        startActivity(homeActivity);
+        finish();
     }
 }
---
+++ app/src/main/java/btventures/ledger/tableview/model/ColumnHeaderModel.java
@@ -0,0 +1,18 @@
+package btventures.ledger.tableview.model;
+
+/**
+ * Created by evrencoskun on 27.11.2017.
+ */
+
+public class ColumnHeaderModel {
+
+    private String mData;
+
+    public ColumnHeaderModel(String mData) {
+        this.mData = mData;
+    }
+
+    public String getData() {
+        return mData;
+    }
+}
